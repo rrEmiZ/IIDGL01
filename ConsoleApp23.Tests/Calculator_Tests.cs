c23f5efba2716ce76a1c7d505da0ac6f319a2614
Jakub Jońca <jakubjonca@outlook.com>
2021-12-08 15:03:48 +0100

    Initial

---
+++ ConsoleApp23.Tests/Calculator_Tests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ConsoleApp23.Tests
+{
+    public class Calculator_Tests
+    {
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(-1, 2, 1)]
+        [InlineData(-1, -2, -3)]
+        public void Add_for_a_and_b_returns_c(int a, int b, int c)
+        {
+            //Act
+            var calc = new Calculator();
+            int result = calc.Add(a, b);
+
+            //Assert
+            Assert.Equal(c, result);
+        }
+
+
+
+        [Fact]
+        public void Add_for_1_and_2_returns_3()
+        {
+            //Parameters
+            int a = 1;
+            int b = 2;
+            int c = 3;
+
+            //Act
+            var calc = new Calculator();
+            int result = calc.Add(a, b);
+
+            //Assert
+            Assert.Equal(c, result);
+        }
+
+        [Fact]
+        public void Add_for_N1_and_2_returns_1()
+        {
+            //Parameters
+            int a = -1;
+            int b = 2;
+            int c = 1;
+
+            //Act
+            var calc = new Calculator();
+            int result = calc.Add(a, b);
+
+            //Assert
+            Assert.Equal(c, result);
+        }
+    }
+}
---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Reflection;
+
+namespace ConsoleApp23
+{
+    public class SomeClass
+    {
+        public string Text { get; set; }
+
+        public void PrintText(string text2)
+        {
+            Console.WriteLine(text2 + " " + Text);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sc = new SomeClass();
+            sc.Text = "Testowy text";
+            sc.PrintText("normal");
+
+            //Blad
+            //sc = "Test";
+
+            //var type = sc.GetType();
+            //var type = typeof(SomeClass);
+
+            dynamic sc2 = new SomeClass();
+            sc2.Text = "Dynamic text";
+            sc2.PrintText("dynamic");
+            Console.WriteLine(sc2.GetType());
+            sc2 = "Test";
+            Console.WriteLine(sc2.GetType());
+            sc2 = 3.2;
+            Console.WriteLine(sc2.GetType());
+
+            object sc3 = new SomeClass();
+            ((SomeClass)sc3).Text = "Object text";
+            ((SomeClass)sc3).PrintText("object");
+            Console.WriteLine(sc3.GetType());
+            sc3 = "Test";
+            Console.WriteLine(sc3.GetType());
+            sc3 = 3.2;
+            Console.WriteLine(sc3.GetType());
+            Console.ReadLine();
+        }
+
+
+           //var sc = new SomeClass();
+           // sc.Text = "Testowy text";
+           // sc.PrintText("normal");
+
+           // //var type = sc.GetType();
+           // //var type = typeof(SomeClass);
+
+       
+
+           // PropertyInfo propInfo = type.GetProperty("Text");
+           // propInfo.SetValue(obj, "Text2");
+
+           // MethodInfo methodInfo = type.GetMethod("PrintText");
+           // methodInfo.Invoke(obj, new[] { "reflection" });
+    }
+}
